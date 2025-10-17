96bf7ba5c785b16b62f26a7c9d1f55495623d343
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-17 16:23:05 +0900

    feat: update customDelimiter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import { Console } from '@woowacourse/mission-utils';
 function calculate(input) {
   if (!input) return 0;
 
-  const splitInput = input.split(/[,:]/);
+  let delimiter = /[,:]/;
+  let numberString = input;
+
+  if (input.startsWith('//')) {
+    const newlineIndex = input.indexOf('\\n');
+
+    if (newlineIndex === -1) {
+      throw new Error('[ERROR] 커스텀 구분자 형식이 올바르지 않습니다.\n');
+    }
+
+    const customDelimiter = input.substring(2, newlineIndex);
+    numberString = input.substring(newlineIndex + 2);
+    delimiter = customDelimiter;
+  }
+
+  const splitInput = numberString.split(delimiter);
 
   const sum = splitInput.reduce((currentSum, stringInput) => {
     const num = Number(stringInput);
-
     if (isNaN(num)) throw new Error('숫자를 입력해 주세요.\n');
     if (num < 0) throw new Error('양수를 입력해 주세요.\n');
 
